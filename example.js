0d00e3ec298df0ccb06941001aa207af8e89da72
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-11-23 08:51:13 +0100

    Remove example

--- example.js
+++
@@ -1,9 +0,0 @@
-const fs = require('./')
-
-const rs = fs.createReadStream('index.js')
-const ws = fs.createWriteStream('index.copy.js')
-
-rs.pipe(ws).on('close', function () {
-  console.log('copied file')
-  fs.readFile('index.copy.js', console.log)
-})
